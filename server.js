e3b913b1f0261205afe1facd7be89f918d242cd5
Gareth Down <g@garethdown.com>
2016-12-23 11:17:43 +0000

    swap for java

--- server.js
+++
@@ -1,6 +0,0 @@
-var vertx = require('vertx');
-var container = require('vertx/container');
-
-vertx.createHttpServer().requestHandler(function(req) {
-    req.response.end('Hello from Vert.x 2.1.5!');
-}).listen(parseInt(container.env.get('PORT')));
